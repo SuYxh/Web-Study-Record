1f6576f14c33c5d9b7a66b6180e3523a3d4b38f1
P0159775 <xinhao.yang@pactera.com>
2020-07-17 20:57:23 +0800

    202007172100

---
+++ js/res.js
@@ -0,0 +1,10 @@
+// 1.如何将浮点数点左边的数每三位添加一个逗号，如12000000.11转化为『12,000,000.11』?
+function commafy(num){
+  return num && num
+    .toString()
+    .replace(/(\d)(?=(\d{3})+\.)/g, function($1, $2){
+      return $2 + ',';
+    });
+}
+
+console.log(commafy(12000000.11))
